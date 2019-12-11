1e3a58e4961008e24bf426af96b7983af3aa5377
Uditi <uditiarora@gmail.com>
2019-12-11 20:20:13 +0530

    11

---
+++ ModifiedPackConsequetiveDuplicates.scala
@@ -0,0 +1,29 @@
+
+
+object ModifiedPackConsequetiveDuplicates {
+  def pack[A](l: List[A]):List[List[A]] = {
+    def _pack(res: List[List[A]], rem: List[A]):List[List[A]] = rem match {
+        case Nil => res
+        case ls => {
+            val (s: List[A], r: List[A]) = rem span { _ == rem.head }
+            _pack(res:::List(s), r)
+        }
+    }
+    _pack(List(), l)
+  }
+   def encode[A](l: List[A]):List[Either[A, (Int, A)]] = {
+    pack(l) map { e => e match{
+      case e if(e.length==1) => Left(e.head)
+      case e => Right((e.length, e.head))
+
+      }
+    }
+  }
+
+   def main(args: Array[String]): Unit = {
+    val list = List(1,1,2,3,4,5,6,6);
+    print(list);
+    print("\n");
+    print(encode(list));
+  }
+}
