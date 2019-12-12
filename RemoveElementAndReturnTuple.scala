6baab107326510983872b4d01cd42b404bf4eb75
Uditi <uditiarora@gmail.com>
2019-12-12 09:37:31 +0530

    20

---
+++ RemoveElementAndReturnTuple.scala
@@ -0,0 +1,13 @@
+
+
+object RemoveElementAndReturnTuple {
+  def remove[A](n : Int, l : List[A]) : (List[A],A) = {
+    (l.take(n):::l.takeRight(l.length-n),l(n))
+  }
+  def main(args: Array[String]): Unit = {
+    val list = List(1,2,3,4,4,5);
+    print(list)
+    print("\n")
+    print(remove(5,list))
+  }
+}
